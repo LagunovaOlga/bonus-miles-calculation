25eb41330f464ca2edcc7548a4a8f1f8a2f33348
Olga Lagunova <skynet-irbit@yandex.ru>
2022-02-09 00:03:15 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 9500;
+        int costs = 20;
+
+        int numberOfMiles = ticketPrice / costs;
+        System.out.println("Колличество бонусных миль " + numberOfMiles);
+    }
+}
